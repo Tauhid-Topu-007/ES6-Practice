26e60441332a0fd6033d694fa507e4fcd7f64c90
tauhid-topu <t.topu021@gmail.com>
2024-08-21 00:59:52 +0600

    first five

---
+++ ES6/letConst.js
@@ -0,0 +1,28 @@
+const hubby="Topu";
+console.log(hubby);
+
+const numbers= [12,45];
+numbers[1]=88;
+numbers.push(55);
+
+const nayok={name:'Topu',Phone:01400522994}
+console.log(numbers);
+console.log(nayok);
+
+let patientName="Rahim Chacha";
+patientName='Jara'
+console.log(patientName);
+
+let sum=0;
+for(let i=0;i<10;i++){
+    sum+=i;
+}
+console.log(i);
+
+
+
+
+
+
+
+
---
+++ ES6/template.js
@@ -0,0 +1,18 @@
+const firstName='Tauhid';
+const lastName='Topu';
+const fullName= firstName+" "+lastName;
+const fullName2= `${firstName} ${lastName} is a good boy`;
+console.log(fullName);
+console.log(fullName2);
+
+
+const multiLine="I love you\n"
+    +"i Miss You\n"
+    +"i hug you";
+console.log(multiLine);
+
+const multiLine2= `I love you 
+i miss you
+i hug you
+no need your sympathy`
+console.log(multiLine2);
